7e54d7d801467aa34cf4eae061c142b39c365f32
Chukobyte <michaelsmith8293@gmail.com>
2024-03-28 23:46:14 -0400

    Getting closer.

--- examples/zig_example/build.zig
+++ examples/zig_example/build.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const libDir = b.option([]const u8, "libDir", "Library directory path");
+    const includeDir = b.option([]const u8, "includeDir", "Include directory path");
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
@@ -9,6 +12,11 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    exe.linkLibC();
+    exe.addIncludePath(.{ .path = includeDir.? });
+    exe.addLibraryPath(.{ .path = libDir.? });
+    exe.linkSystemLibrary("seika");
 
     b.installArtifact(exe);
 }
--- examples/zig_example/src/main.zig
+++ examples/zig_example/src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const seika = @cImport({
+    @cInclude("seika.h");
+});
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
+
+    const success = seika.ska_init_all("Simple Window", 800, 600, 800, 600);
+    if (!success) {
+        std.debug.print("Failed to initialize seika\n", .{});
+        return;
+    }
 }
