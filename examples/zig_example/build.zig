9c15d5453069205f3f07b1578484a60c4a7dd686
Chukobyte <michaelsmith8293@gmail.com>
2024-03-31 19:42:59 -0400

    More updates.

--- examples/zig_example/build.zig
+++ examples/zig_example/build.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
 
+pub fn addBuildLibrary(exe: *std.Build.Step.Compile, includePath: []const u8, libPath: []const u8, linkFlags: []const u8) void {
+    exe.addLibraryPath(.{ .path = libPath });
+    exe.addIncludePath(.{ .path = includePath });
+    var it = std.mem.split(u8, linkFlags, " ");
+    while (it.next()) |flag| {
+        exe.linkSystemLibrary(flag);
+    }
+}
+
 pub fn build(b: *std.Build) void {
+    const fetchContentDir = b.option([]const u8, "fetchContentDir", "Fetch content path for dependencies");
+    const thirdpartyDir = b.option([]const u8, "thirdpartyDir", "Thirdpart path for dependencies");
     const libDirs = b.option([]const u8, "libDirs", "Library directory path");
     const includeDirs = b.option([]const u8, "includeDirs", "Include directory path");
+
+    const allocator = std.heap.page_allocator;
 
     const target = b.standardTargetOptions(.{});
 
@@ -16,19 +29,33 @@ pub fn build(b: *std.Build) void {
     });
 
     // Split the paths and add them
-    var libIt = std.mem.split(u8, libDirs.?, "+");
-    while (libIt.next()) |dir| {
-        exe.addLibraryPath(.{ .path = dir });
+    if (libDirs != null and libDirs.?.len > 0) {
+        var libIt = std.mem.split(u8, libDirs.?, "+");
+        while (libIt.next()) |dir| {
+            exe.addLibraryPath(.{ .path = dir });
+        }
     }
 
-    var includeIt = std.mem.split(u8, includeDirs.?, "+");
-    while (includeIt.next()) |dir| {
-        exe.addIncludePath(.{ .path = dir });
+    if (includeDirs != null and includeDirs.?.len > 0) {
+        var includeIt = std.mem.split(u8, includeDirs.?, "+");
+        while (includeIt.next()) |dir| {
+            exe.addIncludePath(.{ .path = dir });
+        }
     }
 
 
     exe.linkLibC();
-    exe.linkSystemLibrary("pthreadGC2");
+
+    // pthread (GC2)
+    const pthreadIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{thirdpartyDir.?, "pthreads-win32/include"}) catch "Format failed";
+    const pthreadLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{thirdpartyDir.?, "pthreads-win32/lib/x64"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        pthreadIncludeDir,
+        pthreadLibDir,
+        "pthreadGC2"
+    );
+
     exe.linkSystemLibrary("gcc_eh");
     exe.linkSystemLibrary("gdi32");
     exe.linkSystemLibrary("user32");
@@ -40,14 +67,66 @@ pub fn build(b: *std.Build) void {
     exe.linkSystemLibrary("cfgmgr32");
     exe.linkSystemLibrary("oleaut32");
     exe.linkSystemLibrary("ws2_32");
-    exe.linkSystemLibrary("mingw32");
-    exe.linkSystemLibrary("freetype");
-    exe.linkSystemLibrary("SDL3");
-    exe.linkSystemLibrary("glad");
-    exe.linkSystemLibrary("zip");
-    exe.linkSystemLibrary("stb_image");
-    exe.linkSystemLibrary("seika");
-    // exe.linkSystemLibrary("cglm");
+
+    // freetype
+    const freetypeIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "freetype_content-src/include"}) catch "Format failed";
+    const freetypeLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "freetype_content-build"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        freetypeIncludeDir,
+        freetypeLibDir,
+        if (optimize != std.builtin.Mode.Debug) "freetyped" else "freetype"
+    );
+
+    // SDL3
+    const sdlIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "sdl_content-src/include"}) catch "Format failed";
+    const sdlLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "sdl_content-build"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        sdlIncludeDir,
+        sdlLibDir,
+        "SDL3"
+    );
+
+    // glad
+    const gladIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-build"}) catch "Format failed";
+    const gladLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-src/thirdparty"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        gladIncludeDir,
+        gladLibDir,
+        "glad"
+    );
+
+    // zip
+    const zipIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "kuba_zip_content-src/src"}) catch "Format failed";
+    const zipLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "kuba_zip_content-build"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        zipIncludeDir,
+        zipLibDir,
+        "zip"
+    );
+
+    // stb_image
+    const stbImageIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-src/thirdparty"}) catch "Format failed";
+    const stbImageLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-build"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        stbImageIncludeDir,
+        stbImageLibDir,
+        "stb_image"
+    );
+
+    // seika
+    const seikaIncludeDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-src"}) catch "Format failed";
+    const seikaLibDir = std.fmt.allocPrint(allocator, "{s}{s}", .{fetchContentDir.?, "seika_content-build"}) catch "Format failed";
+    addBuildLibrary(
+        exe,
+        seikaIncludeDir,
+        seikaLibDir,
+        "seika"
+    );
 
     b.installArtifact(exe);
 }
--- examples/zig_example/src/main.zig
+++ examples/zig_example/src/main.zig
@@ -4,8 +4,6 @@ const seika = @cImport({
 });
 
 pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
-
     const success = seika.ska_init_all("Zig Window", 800, 600, 800, 600);
     if (!success) {
         std.debug.print("Failed to initialize seika\n", .{});
